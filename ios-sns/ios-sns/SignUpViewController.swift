7738b0c91df6cad5c36762f7f86808712f7b8964
mmihye <smilehye910@naver.com>
2023-06-17 22:24:57 +0900

    Sign Up View

---
+++ ios-sns/ios-sns/SignUpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignUpViewController.swift
+//  ios-sns
+//
+//  Created by 석미혜 on 2023/06/17.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
